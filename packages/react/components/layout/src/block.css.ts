35079aee273951f582231724706dd93012510139
hjban <baneju212@gmail.com>
2024-02-04 18:08:12 +0900

    feat: 레이아웃 패키지 제작 및 세팅

---
+++ packages/react/components/layout/src/block.css.ts
@@ -0,0 +1,8 @@
+import { vars } from "@custom-design-system/themes";
+import { style } from "@vanilla-extract/css";
+
+export const blockStyle = style({
+  width: "100px",
+  height: "100px",
+  backgroundColor: vars.colors.$scale.blue[500],
+});
