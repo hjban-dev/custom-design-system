cb8fba2a094e4eac9643bf7aa1dfdbd54b53580e
hjban <baneju212@gmail.com>
2024-02-25 14:32:31 +0900

    feat: AddonInput 컴포넌트 제작

--- services/storybook/stories/ReactComponents/Input/Input.stories.tsx
+++ services/storybook/stories/ReactComponents/Input/Input.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "@custom-design-system/react-components-input/style.css";
-import { Input } from "@custom-design-system/react-components-input";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@custom-design-system/react-components-input";
 
 export default {
   title: "React Components/Input",
@@ -11,5 +15,43 @@ export default {
 };
 
 export const InputStory = {
-  render: () => <Input placeholder="dd" />,
+  render: () => (
+    <>
+      <Input placeholder="dd" size={"xs"} />
+      <Input placeholder="dd" size={"sm"} />
+      <Input placeholder="dd" size={"md"} />
+      <Input placeholder="dd" size={"lg"} />
+    </>
+  ),
+};
+
+export const InputStoryWithDisabled = {
+  render: () => (
+    <>
+      <Input placeholder="dd" isDisabled />
+    </>
+  ),
+};
+
+export const AddonInputGroupStoryWithSizes = {
+  render: () => (
+    <>
+      <InputGroup size="xs" color="red">
+        <InputLeftAddon>$</InputLeftAddon>
+        <Input placeholder="dd" />
+      </InputGroup>
+      <InputGroup size="sm" color="red">
+        <InputLeftAddon>$</InputLeftAddon>
+        <Input placeholder="dd" />
+      </InputGroup>
+      <InputGroup size="md" color="red">
+        <InputLeftAddon>$</InputLeftAddon>
+        <Input placeholder="dd" />
+      </InputGroup>
+      <InputGroup size="lg" color="red">
+        <InputLeftAddon>$</InputLeftAddon>
+        <Input placeholder="dd" />
+      </InputGroup>
+    </>
+  ),
 };
