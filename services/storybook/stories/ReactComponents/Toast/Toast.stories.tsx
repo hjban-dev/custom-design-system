96cb5ae5d6d8b7234a6350712ce183e24913342e
hjban <baneju212@gmail.com>
2024-02-25 20:25:38 +0900

    feat: Toast Provider 제작

---
+++ services/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
@@ -0,0 +1,43 @@
+import "@custom-design-system/react-components-button/style.css";
+import { Button } from "@custom-design-system/react-components-button";
+import React from "react";
+
+import {
+  ToastProvider,
+  useToast,
+} from "@custom-design-system/react-components-toast";
+import "@custom-design-system/react-components-toast/style.css";
+
+export default {
+  title: "React Components/Toast",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+const Example = () => {
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          payload: {
+            message: "Hello, World!",
+          },
+        })
+      }
+    >
+      Toast Button
+    </Button>
+  );
+};
+
+export const ButtonStory = {
+  render: () => (
+    <ToastProvider>
+      <Example />
+    </ToastProvider>
+  ),
+};
